76e9ae872156a3f32394449c0f3b8343414db1f6
Daniil <azbukin2003@gmail.com>
2023-10-15 20:53:26 +0300

    sdas

--- lab_3.py
+++ lab_3.py
@@ -4,7 +4,8 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import math
-
+from scipy.stats import norm
+import scipy as sc
 
 #---------------- TASK 1 --------------------
 def rasp_plot(o=2.0, m=10.0):
@@ -16,7 +17,7 @@ def rasp_plot(o=2.0, m=10.0):
     x_arr.sort()
     for x in x_arr:
         y = (1 / (o * (2 * math.pi)**(1/2))) * math.e**((-(x-m)**2) / 2 * o ** 2)
-        y_arr.append(norm_zak_rasp(y, o, m))
+        y_arr.append(y)
     return x_arr, y_arr
 
 
@@ -40,8 +41,8 @@ plt.show()
 def norm_zak_rasp(x, o=2.0, m=10.0):
     return (1 / (o * (2 * math.pi)**(1/2))) * math.e**((-(x-m)**2) / 2 * o ** 2)
 
-def gen_x_y(o=2.0, m=10.0):
-    N = 10**3
+def gen_x_y(N, o=2.0, m=10.0):
+    N = 10**2
     x_arr = []
     y_arr = []
     for i in range(0, N):
@@ -49,9 +50,33 @@ def gen_x_y(o=2.0, m=10.0):
         x_arr.append(x)
     x_arr.sort()
     for x in x_arr:
-        y = (1 / (o * (2 * math.pi)**(1/2))) * math.e**((-(x-m)**2) / 2 * o ** 2)
-        y_arr.append(norm_zak_rasp(y, o, m))
+        y_arr.append(norm_zak_rasp(x))
     return x_arr, y_arr
 
-#------------------ TASK 3 ------------------
+x_arr_e2, y_arr_e2 = gen_x_y(10**2)
 
+plt.figure(figsize=(12, 7))
+plt.plot(x_arr_e2, y_arr_e2, 'o-r', alpha=0.7, label="first", lw=5, mec='b', mew=2, ms=5)
+plt.title("o=2 m=10 N=10^2")
+plt.grid(True)
+plt.show()
+#------------------ TASK 3 ------------------
+def reverse_fnc(x_arr):
+    o=2
+    m=10
+    func = lambda t: math.e**((-(t-m)**2)/2*(o**2))
+    rev_y = []
+    for x in x_arr:
+        tmp = sc.integrate.quad(func, -math.inf, x)
+        print(tmp)
+        rev_y.append((1 / (o * (2 * math.pi)**(1/2)))*tmp[1])
+    rev_y.sort()
+    return rev_y
+
+y_rev = reverse_fnc(x_arr_e2)
+
+plt.figure(figsize=(12, 7))
+plt.plot(x_arr_e2, y_rev, 'o-r', alpha=0.7, label="first", lw=5, mec='b', mew=2, ms=5)
+plt.title("o=2 m=10 N=10^2")
+plt.grid(True)
+plt.show()
